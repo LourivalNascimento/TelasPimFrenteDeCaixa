897a94edd6649144bbaaeb87ad201c99e5c69cd5
logra <logra@CYBERLOGRA>
2024-10-06 21:39:39 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace TelasPimFrenteDeCaixa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbSenha = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnEsc = new System.Windows.Forms.Button();
+            this.labelNomeUsuario = new System.Windows.Forms.Label();
+            this.labelTotalCompra = new System.Windows.Forms.Label();
+            this.labelCodProd = new System.Windows.Forms.Label();
+            this.labelQTD = new System.Windows.Forms.Label();
+            this.labelCodBarras = new System.Windows.Forms.Label();
+            this.tbQTD = new System.Windows.Forms.TextBox();
+            this.tbCodProd = new System.Windows.Forms.TextBox();
+            this.tbCodBarras = new System.Windows.Forms.TextBox();
+            this.btnCancelarItem = new System.Windows.Forms.Button();
+            this.btnCancelarCompra = new System.Windows.Forms.Button();
+            this.btnEncerrarCompra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbUser
+            // 
+            this.tbUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(251)))), ((int)(((byte)(231)))));
+            this.tbUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbUser.Font = new System.Drawing.Font("Berlin Sans FB", 15.75F, System.Drawing.FontStyle.Bold);
+            this.tbUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(65)))), ((int)(((byte)(194)))));
+            this.tbUser.Location = new System.Drawing.Point(576, 349);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(216, 26);
+            this.tbUser.TabIndex = 7;
+            this.tbUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbSenha
+            // 
+            this.tbSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(251)))), ((int)(((byte)(231)))));
+            this.tbSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbSenha.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSenha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(65)))), ((int)(((byte)(194)))));
+            this.tbSenha.Location = new System.Drawing.Point(576, 426);
+            this.tbSenha.Name = "tbSenha";
+            this.tbSenha.PasswordChar = '*';
+            this.tbSenha.Size = new System.Drawing.Size(216, 27);
+            this.tbSenha.TabIndex = 6;
+            this.tbSenha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.AutoSize = true;
+            this.btnEntrar.BackColor = System.Drawing.Color.Transparent;
+            this.btnEntrar.BackgroundImage = global::TelasPimFrenteDeCaixa.Properties.Resources.btnEntrarNormal;
+            this.btnEntrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEntrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEntrar.FlatAppearance.BorderSize = 0;
+            this.btnEntrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnEntrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnEntrar.Location = new System.Drawing.Point(608, 472);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(140, 57);
+            this.btnEntrar.TabIndex = 5;
+            this.btnEntrar.UseVisualStyleBackColor = false;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            this.btnEntrar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnEntrar_MouseDown);
+            this.btnEntrar.MouseLeave += new System.EventHandler(this.btnEntrar_MouseLeave);
+            this.btnEntrar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnEntrar_MouseMove);
+            // 
+            // btnEsc
+            // 
+            this.btnEsc.AutoSize = true;
+            this.btnEsc.BackColor = System.Drawing.Color.Transparent;
+            this.btnEsc.BackgroundImage = global::TelasPimFrenteDeCaixa.Properties.Resources.btnEsc;
+            this.btnEsc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEsc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEsc.FlatAppearance.BorderSize = 0;
+            this.btnEsc.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnEsc.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnEsc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEsc.ForeColor = System.Drawing.Color.Transparent;
+            this.btnEsc.Location = new System.Drawing.Point(1211, 61);
+            this.btnEsc.Name = "btnEsc";
+            this.btnEsc.Size = new System.Drawing.Size(104, 52);
+            this.btnEsc.TabIndex = 8;
+            this.btnEsc.UseVisualStyleBackColor = false;
+            this.btnEsc.Visible = false;
+            this.btnEsc.Click += new System.EventHandler(this.btnEsc_Click);
+            this.btnEsc.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnEsc_MouseDown);
+            this.btnEsc.MouseLeave += new System.EventHandler(this.btnEsc_MouseLeave);
+            this.btnEsc.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnEsc_MouseMove);
+            // 
+            // labelNomeUsuario
+            // 
+            this.labelNomeUsuario.AutoSize = true;
+            this.labelNomeUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.labelNomeUsuario.Font = new System.Drawing.Font("Berlin Sans FB", 15.75F, System.Drawing.FontStyle.Bold);
+            this.labelNomeUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(65)))), ((int)(((byte)(194)))));
+            this.labelNomeUsuario.Location = new System.Drawing.Point(785, 335);
+            this.labelNomeUsuario.Name = "labelNomeUsuario";
+            this.labelNomeUsuario.Size = new System.Drawing.Size(69, 26);
+            this.labelNomeUsuario.TabIndex = 9;
+            this.labelNomeUsuario.Text = "label1";
+            this.labelNomeUsuario.Visible = false;
+            // 
+            // labelTotalCompra
+            // 
+            this.labelTotalCompra.AutoSize = true;
+            this.labelTotalCompra.BackColor = System.Drawing.Color.Transparent;
+            this.labelTotalCompra.Font = new System.Drawing.Font("Berlin Sans FB Demi", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotalCompra.Location = new System.Drawing.Point(524, 604);
+            this.labelTotalCompra.Name = "labelTotalCompra";
+            this.labelTotalCompra.Size = new System.Drawing.Size(73, 31);
+            this.labelTotalCompra.TabIndex = 10;
+            this.labelTotalCompra.Text = "0,00";
+            this.labelTotalCompra.Visible = false;
+            // 
+            // labelCodProd
+            // 
+            this.labelCodProd.AutoSize = true;
+            this.labelCodProd.BackColor = System.Drawing.Color.Transparent;
+            this.labelCodProd.Font = new System.Drawing.Font("Berlin Sans FB Demi", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCodProd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(65)))), ((int)(((byte)(194)))));
+            this.labelCodProd.Location = new System.Drawing.Point(784, 405);
+            this.labelCodProd.Name = "labelCodProd";
+            this.labelCodProd.Size = new System.Drawing.Size(129, 24);
+            this.labelCodProd.TabIndex = 11;
+            this.labelCodProd.Text = "COD. PROD.";
+            this.labelCodProd.Visible = false;
+            // 
+            // labelQTD
+            // 
+            this.labelQTD.AutoSize = true;
+            this.labelQTD.BackColor = System.Drawing.Color.Transparent;
+            this.labelQTD.Font = new System.Drawing.Font("Berlin Sans FB Demi", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQTD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(65)))), ((int)(((byte)(194)))));
+            this.labelQTD.Location = new System.Drawing.Point(1104, 405);
+            this.labelQTD.Name = "labelQTD";
+            this.labelQTD.Size = new System.Drawing.Size(58, 24);
+            this.labelQTD.TabIndex = 12;
+            this.labelQTD.Text = "QTD.";
+            this.labelQTD.Visible = false;
+            // 
+            // labelCodBarras
+            // 
+            this.labelCodBarras.AutoSize = true;
+            this.labelCodBarras.BackColor = System.Drawing.Color.Transparent;
+            this.labelCodBarras.Font = new System.Drawing.Font("Berlin Sans FB Demi", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCodBarras.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(65)))), ((int)(((byte)(194)))));
+            this.labelCodBarras.Location = new System.Drawing.Point(947, 498);
+            this.labelCodBarras.Name = "labelCodBarras";
+            this.labelCodBarras.Size = new System.Drawing.Size(143, 24);
+            this.labelCodBarras.TabIndex = 13;
+            this.labelCodBarras.Text = "COD. BARRAS";
+            this.labelCodBarras.Visible = false;
+            // 
+            // tbQTD
+            // 
+            this.tbQTD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(223)))), ((int)(((byte)(255)))));
+            this.tbQTD.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbQTD.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbQTD.Location = new System.Drawing.Point(1104, 456);
+            this.tbQTD.Name = "tbQTD";
+            this.tbQTD.Size = new System.Drawing.Size(100, 31);
+            this.tbQTD.TabIndex = 14;
+            this.tbQTD.Visible = false;
+            // 
+            // tbCodProd
+            // 
+            this.tbCodProd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(223)))), ((int)(((byte)(255)))));
+            this.tbCodProd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbCodProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCodProd.Location = new System.Drawing.Point(796, 456);
+            this.tbCodProd.Name = "tbCodProd";
+            this.tbCodProd.Size = new System.Drawing.Size(100, 31);
+            this.tbCodProd.TabIndex = 14;
+            this.tbCodProd.Visible = false;
+            // 
+            // tbCodBarras
+            // 
+            this.tbCodBarras.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(223)))), ((int)(((byte)(255)))));
+            this.tbCodBarras.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbCodBarras.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCodBarras.Location = new System.Drawing.Point(966, 535);
+            this.tbCodBarras.Name = "tbCodBarras";
+            this.tbCodBarras.Size = new System.Drawing.Size(100, 31);
+            this.tbCodBarras.TabIndex = 16;
+            this.tbCodBarras.Visible = false;
+            // 
+            // btnCancelarItem
+            // 
+            this.btnCancelarItem.AutoSize = true;
+            this.btnCancelarItem.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancelarItem.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancelarItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelarItem.Font = new System.Drawing.Font("Berlin Sans FB Demi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelarItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(65)))), ((int)(((byte)(194)))));
+            this.btnCancelarItem.Location = new System.Drawing.Point(772, 589);
+            this.btnCancelarItem.Name = "btnCancelarItem";
+            this.btnCancelarItem.Size = new System.Drawing.Size(107, 48);
+            this.btnCancelarItem.TabIndex = 17;
+            this.btnCancelarItem.Text = "CANCELAR\r\nITEM";
+            this.btnCancelarItem.UseVisualStyleBackColor = false;
+            this.btnCancelarItem.Visible = false;
+            // 
+            // btnCancelarCompra
+            // 
+            this.btnCancelarCompra.AutoSize = true;
+            this.btnCancelarCompra.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancelarCompra.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancelarCompra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelarCompra.Font = new System.Drawing.Font("Berlin Sans FB Demi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelarCompra.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(65)))), ((int)(((byte)(194)))));
+            this.btnCancelarCompra.Location = new System.Drawing.Point(940, 589);
+            this.btnCancelarCompra.Name = "btnCancelarCompra";
+            this.btnCancelarCompra.Size = new System.Drawing.Size(107, 48);
+            this.btnCancelarCompra.TabIndex = 18;
+            this.btnCancelarCompra.Text = "CANCELAR\r\nCOMPRA";
+            this.btnCancelarCompra.UseVisualStyleBackColor = false;
+            this.btnCancelarCompra.Visible = false;
+            // 
+            // btnEncerrarCompra
+            // 
+            this.btnEncerrarCompra.AutoSize = true;
+            this.btnEncerrarCompra.BackColor = System.Drawing.Color.Transparent;
+            this.btnEncerrarCompra.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEncerrarCompra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEncerrarCompra.Font = new System.Drawing.Font("Berlin Sans FB Demi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncerrarCompra.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(65)))), ((int)(((byte)(194)))));
+            this.btnEncerrarCompra.Location = new System.Drawing.Point(1104, 590);
+            this.btnEncerrarCompra.Name = "btnEncerrarCompra";
+            this.btnEncerrarCompra.Size = new System.Drawing.Size(107, 48);
+            this.btnEncerrarCompra.TabIndex = 19;
+            this.btnEncerrarCompra.Text = "ENCERRAR\r\nCOMPRA";
+            this.btnEncerrarCompra.UseVisualStyleBackColor = false;
+            this.btnEncerrarCompra.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = global::TelasPimFrenteDeCaixa.Properties.Resources.VENDAS___CAIXA;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1357, 691);
+            this.Controls.Add(this.btnEncerrarCompra);
+            this.Controls.Add(this.btnCancelarCompra);
+            this.Controls.Add(this.btnCancelarItem);
+            this.Controls.Add(this.tbCodBarras);
+            this.Controls.Add(this.tbCodProd);
+            this.Controls.Add(this.tbQTD);
+            this.Controls.Add(this.labelCodBarras);
+            this.Controls.Add(this.labelQTD);
+            this.Controls.Add(this.labelCodProd);
+            this.Controls.Add(this.labelTotalCompra);
+            this.Controls.Add(this.labelNomeUsuario);
+            this.Controls.Add(this.btnEsc);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.tbSenha);
+            this.Controls.Add(this.btnEntrar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "C A I X A (m o r a n g o l â n d i a)";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbSenha;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnEsc;
+        private System.Windows.Forms.Label labelNomeUsuario;
+        private System.Windows.Forms.Label labelTotalCompra;
+        private System.Windows.Forms.Label labelCodProd;
+        private System.Windows.Forms.Label labelQTD;
+        private System.Windows.Forms.Label labelCodBarras;
+        private System.Windows.Forms.TextBox tbQTD;
+        private System.Windows.Forms.TextBox tbCodProd;
+        private System.Windows.Forms.TextBox tbCodBarras;
+        private System.Windows.Forms.Button btnCancelarItem;
+        private System.Windows.Forms.Button btnCancelarCompra;
+        private System.Windows.Forms.Button btnEncerrarCompra;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TelasPimFrenteDeCaixa
+{
+    public partial class Form1 : Form
+    {
+        private bool BtnEntrarAtivo{ get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //LOGIN 576; 349
+            //Chamando imagem de fundo de login
+            BackgroundImage = Properties.Resources.telaLogin;
+            tbUser.Location = new Point(576, 349);
+            tbSenha.Location = new Point(576, 426);
+            btnEntrar.Location = new Point(608, 472);
+            //VENDAS
+            btnEsc.Location = new Point(1211, 61);
+            labelNomeUsuario.Location = new Point(785, 335);
+
+        }
+
+        //LOGIN
+        private void btnEntrar_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnEntrar.BackgroundImage = Properties.Resources.btnEntrarNormalEscuro;
+        }
+
+        private void btnEntrar_MouseLeave(object sender, EventArgs e)
+        {
+            btnEntrar.BackgroundImage = Properties.Resources.btnEntrarNormal;
+        }
+
+        private void btnEntrar_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnEntrar.BackgroundImage = Properties.Resources.btnEntrarNormal;
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            //Trocando para tela de fundo de vendas
+            BackgroundImage = Properties.Resources.telaVendas;
+            //Ocultando os elementos de login após logar
+            btnEntrar.Hide();
+            tbUser.Hide();
+            tbSenha.Hide();
+            //Elemento de vendas visível
+            btnEsc.Visible = true;
+            btnCancelarCompra.Visible = true;
+            btnCancelarItem.Visible = true;
+            btnEncerrarCompra.Visible = true;
+            tbCodBarras.Visible = true;
+            tbCodProd.Visible = true;
+            tbQTD.Visible = true;
+            labelNomeUsuario.Visible = true;
+            labelCodBarras.Visible = true;
+            labelCodProd.Visible = true;
+            labelQTD.Visible = true;
+            labelTotalCompra.Visible = true;
+            labelNomeUsuario.Text = "Olá " + tbUser.Text + "!";
+        }
+
+        //VENDAS
+        private void btnEsc_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnEsc.BackgroundImage = Properties.Resources.btnEscEscuro;
+        }
+        private void btnEsc_MouseLeave(object sender, EventArgs e)
+        {
+            btnEsc.BackgroundImage = Properties.Resources.btnEsc;
+        }
+        private void btnEsc_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnEsc.BackgroundImage = Properties.Resources.btnEsc;
+        }
+
+        private void btnEsc_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
