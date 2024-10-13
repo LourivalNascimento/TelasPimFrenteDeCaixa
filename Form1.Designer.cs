e3ec27f224fc2e151c4e4cc44a13ea7df7653f57
Lourival Nascimento <lograveld@gmail.com>
2024-10-13 09:49:03 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TelasPimFrenteDeCaixa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureMaca = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureMorango = new System.Windows.Forms.PictureBox();
+            this.pictureCenouraTop = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureLogoMorangolandia = new System.Windows.Forms.PictureBox();
+            this.pictureFundoLogin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMaca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMorango)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCenouraTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogoMorangolandia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFundoLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureMaca
+            // 
+            this.pictureMaca.BackColor = System.Drawing.Color.Transparent;
+            this.pictureMaca.BackgroundImage = global::TelasPimFrenteDeCaixa.Properties.Resources.maçã;
+            this.pictureMaca.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureMaca.Location = new System.Drawing.Point(-50, -78);
+            this.pictureMaca.Name = "pictureMaca";
+            this.pictureMaca.Size = new System.Drawing.Size(304, 316);
+            this.pictureMaca.TabIndex = 0;
+            this.pictureMaca.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(53, 366);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(67, 215);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(327, 393);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(594, 366);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureMorango
+            // 
+            this.pictureMorango.BackColor = System.Drawing.Color.Transparent;
+            this.pictureMorango.BackgroundImage = global::TelasPimFrenteDeCaixa.Properties.Resources.morango;
+            this.pictureMorango.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureMorango.Location = new System.Drawing.Point(484, -64);
+            this.pictureMorango.Name = "pictureMorango";
+            this.pictureMorango.Size = new System.Drawing.Size(254, 249);
+            this.pictureMorango.TabIndex = 5;
+            this.pictureMorango.TabStop = false;
+            // 
+            // pictureCenouraTop
+            // 
+            this.pictureCenouraTop.BackColor = System.Drawing.Color.Transparent;
+            this.pictureCenouraTop.BackgroundImage = global::TelasPimFrenteDeCaixa.Properties.Resources.cenoura_esquerda;
+            this.pictureCenouraTop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureCenouraTop.Location = new System.Drawing.Point(287, -79);
+            this.pictureCenouraTop.Name = "pictureCenouraTop";
+            this.pictureCenouraTop.Size = new System.Drawing.Size(296, 146);
+            this.pictureCenouraTop.TabIndex = 6;
+            this.pictureCenouraTop.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(659, 184);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureLogoMorangolandia
+            // 
+            this.pictureLogoMorangolandia.BackColor = System.Drawing.Color.Transparent;
+            this.pictureLogoMorangolandia.BackgroundImage = global::TelasPimFrenteDeCaixa.Properties.Resources.morangolandia_logo;
+            this.pictureLogoMorangolandia.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureLogoMorangolandia.Location = new System.Drawing.Point(228, 44);
+            this.pictureLogoMorangolandia.Name = "pictureLogoMorangolandia";
+            this.pictureLogoMorangolandia.Size = new System.Drawing.Size(249, 107);
+            this.pictureLogoMorangolandia.TabIndex = 8;
+            this.pictureLogoMorangolandia.TabStop = false;
+            // 
+            // pictureFundoLogin
+            // 
+            this.pictureFundoLogin.BackColor = System.Drawing.Color.Transparent;
+            this.pictureFundoLogin.BackgroundImage = global::TelasPimFrenteDeCaixa.Properties.Resources.fundo_login;
+            this.pictureFundoLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureFundoLogin.Location = new System.Drawing.Point(276, 196);
+            this.pictureFundoLogin.Name = "pictureFundoLogin";
+            this.pictureFundoLogin.Size = new System.Drawing.Size(161, 155);
+            this.pictureFundoLogin.TabIndex = 9;
+            this.pictureFundoLogin.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(706, 450);
+            this.Controls.Add(this.pictureMorango);
+            this.Controls.Add(this.pictureCenouraTop);
+            this.Controls.Add(this.pictureFundoLogin);
+            this.Controls.Add(this.pictureLogoMorangolandia);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureMaca);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMaca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMorango)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCenouraTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogoMorangolandia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFundoLogin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureMaca;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureMorango;
+        private System.Windows.Forms.PictureBox pictureCenouraTop;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureLogoMorangolandia;
+        private System.Windows.Forms.PictureBox pictureFundoLogin;
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("TelasPimFrenteDeCaixa")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("TelasPimFrenteDeCaixa")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("cf88ceed-2c85-4f89-a77e-ead1d7ecee93")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
